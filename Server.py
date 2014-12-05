39271f76248480a11aec7ae4a4cda34c9d521078
Paolo Bruzzo <pbruzz2@uic.edu>
2014-12-04 19:54:21 -0600

    Server wait valid connection

--- Server.py
+++ Server.py
@@ -8,7 +8,7 @@ BUFFER_SIZE = 1024
 SECRET_SHA512 = 'bd2b1aaf7ef4f09be9f52ce2d8d599674d81aa9d6a4421696dc4d93dd0619d682ce56b4d64a9ef097761ced99e0f67265b5f76085e5b0ee7ca4696b2ad6fe2b2'
 
 
-def waitValidConnection():
+def setConnection():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((TCP_IP, TCP_PORT))
     s.listen(1)
@@ -18,7 +18,6 @@ def waitValidConnection():
     data = conn.recv(BUFFER_SIZE)
     password = ''
     while data:
-        print 'Received data: ', data
         password += data
         data = conn.recv(BUFFER_SIZE)
     conn.close()
@@ -28,7 +27,12 @@ def waitValidConnection():
     return False
 
 if __name__ == '__main__':
-    if waitValidConnection():
-        print("Connection Accepted")
-    else:
-        print("Connection Refused")
+
+    valid = setConnection()
+
+    # Wait for a valid connection
+    while not valid:
+        print "Connection Refused"
+        valid = setConnection()
+
+    print "Connection Accepted"
