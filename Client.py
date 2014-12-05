2e5666695da19d5cd6e5f34a452b5811dc8ecf4d
Paolo Bruzzo <pbruzz2@uic.edu>
2014-12-04 20:30:09 -0600

    Introduced secure TLS transport

--- Client.py
+++ Client.py
@@ -3,24 +3,26 @@ __author__ = 'paolobruzzo'
 import socket
 import hashlib
 import getpass
+import ssl
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+TCP_PORT = 8181
 BUFFER_SIZE = 1024
 
 
 def connect(password):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    secure_sock = ssl.wrap_socket(sock, ca_certs='cert.pem', cert_reqs=ssl.CERT_REQUIRED, ssl_version=ssl.PROTOCOL_TLSv1)
 
     # Try to connect to the server
     try:
-        s.connect((TCP_IP, TCP_PORT))
+        secure_sock.connect((TCP_IP, TCP_PORT))
     except socket.error:
         print "Connection Refused"
         return False
 
-    s.send(password)
-    s.close()
+    secure_sock.send(password)
+    secure_sock.close()
 
 
 if __name__ == '__main__':
--- Server.py
+++ Server.py
@@ -1,9 +1,8 @@
-__author__ = 'paolobruzzo'
-
 import socket
+import ssl
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+TCP_PORT = 8181
 BUFFER_SIZE = 1024
 SECRET_SHA512 = 'bd2b1aaf7ef4f09be9f52ce2d8d599674d81aa9d6a4421696dc4d93dd0619d682ce56b4d64a9ef097761ced99e0f67265b5f76085e5b0ee7ca4696b2ad6fe2b2'
 
@@ -13,14 +12,21 @@ def setConnection():
     s.bind((TCP_IP, TCP_PORT))
     s.listen(1)
 
-    conn, addr = s.accept()
+    sock, addr = s.accept()
 
-    data = conn.recv(BUFFER_SIZE)
+    # Create a secure connection with SSL
+    secure_sock = ssl.wrap_socket(sock, server_side=True, certfile='cert.pem', keyfile='cert.pem', ssl_version=ssl.PROTOCOL_TLSv1)
+
+    data = secure_sock.recv(BUFFER_SIZE)
+
+    # Read password sent by the client
     password = ''
     while data:
         password += data
-        data = conn.recv(BUFFER_SIZE)
-    conn.close()
+        data = secure_sock.recv(BUFFER_SIZE)
+
+    # Close connection (TODO: leave the connection open in case of successful connection)
+    secure_sock.close()
 
     if password == SECRET_SHA512:
         return True
